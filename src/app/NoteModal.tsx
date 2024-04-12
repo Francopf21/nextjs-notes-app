a33d2730a145c33fc59d8e680bf69941ae69edf0
Francopf21 <80114308+Francopf21@users.noreply.github.com>
2024-04-12 19:06:16 -0300

    Update NoteModal.tsx

--- src/app/NoteModal.tsx
+++ src/app/NoteModal.tsx
@@ -175,7 +175,7 @@ function NoteModal({
                 value={content}
                 onChange={(e) => setContent(e.target.value)}
                 className="w-full p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Enter your note..."
+                placeholder="Enter your note... HOLA JOSE"
               ></textarea>
             </div>
             <div className="flex justify-end">
